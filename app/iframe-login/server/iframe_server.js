1b6bf78fe11f04d5b4712ab03b690cb2c19c2af8
Vincent Engelmann <vengelmann@ntent.com>
2019-09-21 16:28:38 -0700

    register new iframeTokenParam loginHandler

--- app/iframe-login/server/iframe_server.js
+++ app/iframe-login/server/iframe_server.js
@@ -3,29 +3,46 @@ import { check } from 'meteor/check';
 import { Accounts } from 'meteor/accounts-base';
 import { OAuth } from 'meteor/oauth';
 
-Accounts.registerLoginHandler('iframe', function(result) {
-	if (!result.iframe) {
-		return;
-	}
+Accounts.registerLoginHandler('iframe', function (result) {
+  if (!result.iframe) {
+    return;
+  }
 
-	check(result.token, String);
+  check(result.token, String);
 
-	console.log('[Method] registerLoginHandler');
+  console.log('[Method] registerLoginHandler');
 
-	const user = Meteor.users.findOne({
-		'services.iframe.token': result.token,
-	});
+  const user = Meteor.users.findOne({
+    'services.iframe.token': result.token,
+  });
 
-	if (user) {
-		return {
-			userId: user._id,
-		};
-	}
+  if (user) {
+    return {
+      userId: user._id,
+    };
+  }
+});
+
+Accounts.registerLoginHandler('iframeTokenParam', function (result) {
+  if (!result.iframeTokenParam) {
+    return;
+  }
+
+  let hashedTokenProvided = Accounts._hashLoginToken(result.token);
+  let authedUser = Meteor.users.findOne({
+    "services.resume.loginTokens": { $elemMatch: { "hashedToken": hashedTokenProvided } }
+  });
+  debugger;
+  if (authedUser) {
+    return {
+      userId: authedUser._id
+    };
+  }
 });
 
 
 Meteor.methods({
-	'OAuth.retrieveCredential'(credentialToken, credentialSecret) {
-		return OAuth.retrieveCredential(credentialToken, credentialSecret);
-	},
+  'OAuth.retrieveCredential'(credentialToken, credentialSecret) {
+    return OAuth.retrieveCredential(credentialToken, credentialSecret);
+  },
 });
